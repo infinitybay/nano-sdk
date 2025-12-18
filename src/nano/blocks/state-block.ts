b39d3da1fcd8ac4eaa8cf547b94fa7812b5fd244
Exxenoz <exxenoz@gmx.at>
2025-12-18 22:58:44 +0100

    Add link/link_as_account consistency check to StateBlock

--- src/nano/blocks/state-block.ts
+++ src/nano/blocks/state-block.ts
@@ -1,5 +1,6 @@
 import { z } from "zod";
 
+import { derivePublicKeyFromAccount } from "../crypto";
 import { AccountString } from "../types/account";
 import { RawAmountString } from "../types/amount";
 import { HashString } from "../types/hash";
@@ -8,16 +9,28 @@ import { SignatureString } from "../types/signature";
 import { StateTypeString } from "../types/type";
 import { WorkString } from "../types/work";
 
+function isValidLink(link: LinkString, link_as_account: AccountString): boolean {
+  const publicKeyResult = derivePublicKeyFromAccount({ account: link_as_account, throwOnError: false });
+  if (publicKeyResult.success) {
+    return publicKeyResult.data === link;
+  }
+  return false;
+}
+
 export type StateBlock = z.infer<ReturnType<typeof StateBlock>>;
 export const StateBlock = () =>
-  z.object({
-    type: StateTypeString(),
-    account: AccountString(),
-    previous: HashString(),
-    representative: AccountString(),
-    balance: RawAmountString(),
-    link: LinkString(),
-    link_as_account: AccountString(),
-    signature: SignatureString(),
-    work: WorkString(),
-  });
+  z
+    .object({
+      type: StateTypeString(),
+      account: AccountString(),
+      previous: HashString(),
+      representative: AccountString(),
+      balance: RawAmountString(),
+      link: LinkString(),
+      link_as_account: AccountString(),
+      signature: SignatureString(),
+      work: WorkString(),
+    })
+    .refine((val) => isValidLink(val.link, val.link_as_account), {
+      message: "link must match the public key derived from link_as_account!",
+    });
